a708f29eeab2246402069d5bea84916b9454b54e
FranciscoVPinto <francisco.v.pinto@tecnico.ulisboa.pt>
2025-05-24 18:11:04 +0100

    Final

--- Matlab_Code/GenKinBlock.m
+++ Matlab_Code/GenKinBlock.m
@@ -1,7 +1,6 @@
 clear all
 clc
 
-%-------------------------------------------------------------------------------------------------
 % DK
 
 DH = DHtable();
@@ -11,17 +10,15 @@ A06 = DKin(DH);
 R_tool = A06(1:3,1:3);
 P_tool = A06(1:3,4);
 
-%-------------------------------------------------------------------------------------------------
 % JACOB
 
 syms q1 q2 q3 q4 q5 q6 real
 assume([q1 q2 q3 q4 q5 q6], 'real');
 
-% Posição da tool final
+
 A06 = DKin(DH);
 P_e = A06(1:3,4);
 
-% Inicialização - linear e angular
 Jp = sym(zeros(3,6));
 Jo = sym(zeros(3,6));
 
@@ -38,10 +35,10 @@ for i = 1:6
     Jo(:,i) = Z;
 end
 
-J = [Jp; Jo];  % Jacobiana geométrica
+J = [Jp; Jo]; 
 
 
-%-------------------------------------------------------------------------------------------------
+%BLOCK
 
 new_system('RobotKinematics_Lib');            
 open_system('RobotKinematics_Lib');            
